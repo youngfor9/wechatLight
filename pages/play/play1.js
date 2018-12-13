dc477ee46189f8542e64a1f7ce114ec637db0c2a
youngfortwo <young_for9@yeah.net>
2018-12-13 22:44:00 +0800

    '选择'

--- pages/play/play1.js
+++ pages/play/play1.js
@@ -1,20 +1,19 @@
 // pages/play/play1.js
-const initData = '\nthis is first line\nthis is second line'
+const content = '\n三年又三年'
 const titleNum =1
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-      content:initData,
-      titleNum: titleNum
+    content: content,
+      titleNum: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
   },
 
   /**
@@ -71,7 +70,9 @@ Page({
     })
   },
   nextQuestion:function (e) {
-    
-    this.titleNum =titleNum+1
+    this.data.titleNum = this.data.titleNum +1,
+    this.setData({
+       titleNum: this.data.titleNum,
+    })
   }
 })
