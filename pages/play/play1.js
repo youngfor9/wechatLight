fcddf2d9576fcdac21cc8050cbdbedb6fadb6c05
xiangyang.yang <xiangyang.yang@hdtmedia.com>
2019-08-09 18:16:15 +0800

    添加问题背景

--- pages/play/play1.js
+++ pages/play/play1.js
@@ -4,21 +4,42 @@ var process = require("../play/process.js");
 const autoId = "A";
 const score = 0;
 const ansNum = 3;
-const titleNum =0;
-var app =getApp();
+const titleNum = 0;
+const time = null;
+var app = getApp();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     content: content,
-    anslist: [
+    anslist: [{
+        id: 1,
+        letter: "A",
+        content: "鬼子来了"
+      },
+      {
+        id: 2,
+        letter: "B",
+        content: "精武门"
+      },
+      {
+        id: 3,
+        letter: "C",
+        content: "斗牛"
+      },
+      {
+        id: 4,
+        letter: "D",
+        content: "神话"
+      }
     ],
     titleNum: 1,
     res: ["A", "A", "A"],
     score: 0,
-    is_stop:false,
-    contentArr:null
+    contentArr: null,
+    time: null,
+    view: "pages/image/view1.gif"
   },
 
   /**
@@ -26,7 +47,7 @@ Page({
    */
   onLoad: function(options) {
     var param = {
-      "num": 3 
+      "num": 3
     }
     var _this = this
     wx.request({
@@ -56,7 +77,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-   
+
     this.countDown();
   },
 
@@ -64,7 +85,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.showBackBround();
   },
 
   /**
@@ -102,32 +123,31 @@ Page({
 
   },
   exitGame: function(e) {
-    this.data.is_stop = !this.data.is_stop;
     app.exitGame();
   },
   toScore: function(score) {
     console.info("---" + score);
-    this.data.is_stop =true;
     wx.redirectTo({
       url: '../res/res?score=' + score
     })
   },
-  showNextQuestion:function(){
+  showNextQuestion: function() {
     this.setData({
       titleNum: this.data.titleNum + 1,
     })
     //内容
     console.info("content.size:" + this.data.titleNum);
-    var contentJson = this.data.contentArr[this.data.titleNum-1];
-     console.info("contentJson:" + contentJson);
+    var contentJson = this.data.contentArr[this.data.titleNum - 1];
+    console.info("contentJson:" + contentJson);
     this.setData({
-      content:contentJson.info.content,
-      anslist:contentJson.ans
+      content: contentJson.info.content,
+      anslist: contentJson.ans
     })
     this.countDown();
   },
   nextQuestion: function(e) {
     console.info(e.currentTarget.id);
+    clearInterval(this.data.time);
     this.judgeSelect(e.currentTarget.id);
   },
   //显示选择题
@@ -147,49 +167,41 @@ Page({
   },
 
   countDown: function() {
-    
+    var time = this.data.time;
     var _this = this;
-    var step = 1, //计数动画次数
+    var v=10,step = 10, //计数动画次数
       num = 0, //计数倒计时秒数（n - num）
       start = 1.5 * Math.PI, // 开始的弧度
-      end = -0.5 * Math.PI, // 结束的弧度
-      time = null; // 计时器容器
-    var animation_interval = 1000, // 每1秒运行一次计时器
+      end = -0.5 * Math.PI; // 结束的弧度
+    // 计时器容器
+    var animation_interval = 125, // 每1秒运行一次计时器
       n = 3; // 当前倒计时为10秒
-    // 动画函数
-    
     function animation() {
-      if (step <= n ) {
-        end = end + 2 * Math.PI / n;
+      if (step <= v * (n + 1)) {
+        end = end + 2 * Math.PI / (v * n);
         ringMove(start, end);
         step++;
       } else {
         //自动显示下一题
-        clearInterval(time);
+        clearInterval(_this.data.time);
         console.info("titleNum:" + _this.data.titleNum);
-        if (_this.data.titleNum < ansNum + 1 && !_this.data.is_stop ) {
+        if (_this.data.titleNum < ansNum + 1 ) {
           _this.judgeSelect(autoId);
-          //倒计时的问题待解决
         }
       }
     };
     // 画布绘画函数
     function ringMove(s, e) {
       var context = wx.createCanvasContext('secondCanvas')
-
-      var gradient = context.createLinearGradient(200, 100, 100, 200);
-      gradient.addColorStop("0", "#1cc955");
-      gradient.addColorStop("0.5", "#1cc955");
-      gradient.addColorStop("1.0", "#1cc955");
-
       // 绘制圆环
       context.setStrokeStyle('#1cc955')
       context.beginPath()
       context.setLineWidth(8)
+      context.setLineCap('round')
       context.arc(42, 42, 30, s, e, true)
       context.stroke()
       context.closePath()
-
+      console.info("n:"+n);
       // 绘制倒计时文本
       context.beginPath()
       context.setLineWidth(1)
@@ -197,18 +209,26 @@ Page({
       context.setFillStyle('#1cc955')
       context.setTextAlign('center')
       context.setTextBaseline('middle')
-      context.fillText(n - num + '', 42, 42, 30)
+      context.fillText(n - parseInt(num / v) + '', 42, 42, 30)
       context.fill()
       context.closePath()
-
-      context.draw()
-
       // 每完成一次全程绘制就+1
       num++;
+      context.draw()
     }
     // 倒计时前先绘制整圆的圆环
     ringMove(start, end);
     // 创建倒计时
-      time = setInterval(animation, animation_interval);
+    //time = setInterval(animation, animation_interval);
+    _this.setData({
+      time: time
+    })
+  },
+  showBackBround: function () {
+    var that = this;
+    let bgImage = wx.getFileSystemManager().readFileSync(that.data.view, 'base64')
+    that.setData({
+      'view': 'data:image/jpg;base64,' + bgImage
+    });
   }
 })
